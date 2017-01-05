c744d137adb4944ed96788c4918b60f4b3a9fb21
sameesh-s <sameesh.fisat@gmail.com>
2017-01-06 00:50:53 +0530

    Stack implimentation in class

---
+++ Stack.rb
@@ -0,0 +1,19 @@
+require_relative "stacklike.rb"
+
+#class Stack
+#  include Stacklike
+#end
+
+class Stack
+  attr_reader :stack
+  def initialize
+    @stack = []
+  end
+  def add_to_stack(obj)
+    @stack.push(obj)
+  end
+  def take_from_stack
+    @stack.pop
+  end
+end
+
